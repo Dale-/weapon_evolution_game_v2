86a42d3ab4f2b8a9da3440594560e4dca08f7af2
dale <18717394542@163.com>
2015-01-23 20:50:55 +0800

    create poison_effect.js

---
+++ src/effect/poison-effect.js
@@ -0,0 +1,26 @@
+var _ = require('lodash');
+var NormalState = require('../state/poison-state');
+
+function DeadlyAttackEffect(times, effectTime, damage) {
+    this.times = _.isUndefined(times) ? 2 : times;
+    this.effectTime = _.isUndefined(effectTime) ? 2 : effectTime;
+    this.damage = _.isUndefined(damage) ? 2 : damage;
+}
+
+DeadlyAttackEffect.prototype.trigger = function() {
+    var times = this.times;
+    var effectTime = this.effectTime;
+    var damage = this.damage;
+
+    return {
+        state: new PoisonState(times, effectTime, damage),
+
+        is_damage: false,
+
+        effect_string: function(name) {
+            return name + '中毒了,';
+        }
+    };
+};
+
+module.exports = DeadlyAttackEffect;
