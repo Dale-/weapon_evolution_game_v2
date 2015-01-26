5c1ca9efe04332d358508cd648988f0962b52587
dale <18717394542@163.com>
2015-01-26 18:57:52 +0800

    create frozen-state and test pass

---
+++ __tests__/state/frozen_state_test.js
@@ -0,0 +1,55 @@
+jest.dontMock('lodash');
+jest.dontMock('../../src/state/state');
+jest.dontMock('../../src/model/player');
+jest.dontMock('../../src/state/frozen-state');
+
+describe('FrozenState', function() {
+
+    var FrozenState;
+    var frozenState;
+
+    beforeEach(function() {
+        FrozenState = require('../../src/state/frozen-state');
+        frozenState = new FrozenState(1, 3, 0);
+    });
+
+    describe('#name()', function() {
+        it('should return string of 冰冻', function() {
+            var result = frozenState.name();
+            expect(result).toBe('冰冻');
+        });
+    });
+
+    describe('#merge()', function() {
+        it('should return poison times', function() {
+            var newFrozenState = new FrozenState();
+            var result = frozenState.merge(newFrozenState);
+            expect(frozenState.frozenStatus.length).toBe(2);
+        });
+    });
+
+    describe('#impact()', function() {
+        it('should return string about dizzy impact', function() {
+
+            var Player = require('../../src/model/player');
+            var katherine = new Player('Katherine', 100, 8);
+            frozenState.frozenStatus = [2];
+            var result = frozenState.impact(katherine);
+            expect(result).toBe('平民Katherine冻得直哆嗦，没有击中\n');
+        });
+    });
+
+    describe('#addTimes()', function() {
+        it('should return array that has been add', function() {
+            frozenState.addTimes();
+            expect(frozenState.frozenStatus[0]).toBe(2);
+        });
+    });
+
+    describe('#reduceTimes()', function() {
+        it('should return number that has been reduced', function() {
+            frozenState.reduceTimes();
+            expect(frozenState.frozenStatus.length).toBe(0);
+        });
+    });
+});
---
+++ src/state/frozen-state.js
@@ -0,0 +1,42 @@
+var _ = require('lodash');
+var State = require('./state');
+
+function FrozenState(times, effectTime, damage) {
+    State.call(this, times, effectTime, damage);
+    this.frozenStatus = [1];
+}
+
+FrozenState.prototype = Object.create(State.prototype);
+FrozenState.prototype.constructor = FrozenState;
+
+FrozenState.prototype.name = function() {
+    return '冰冻';
+};
+
+FrozenState.prototype.merge = function(fireState) {
+    this.frozenStatus.unshift(1);
+};
+
+FrozenState.prototype.impact = function(human) {
+    this.addTimes();
+    if(this.frozenStatus && this.frozenStatus[this.frozenStatus.length -1] === this.effectTime) {
+        this.reduceTimes();
+        return human.role() + human.name + '冻得直哆嗦，没有击中\n';
+    } else {
+        return '';
+    }
+};
+
+FrozenState.prototype.addTimes = function() {
+    if(this.frozenStatus) {
+        this.frozenStatus = _.map(this.frozenStatus, function(num) {
+            return num + 1;
+        });
+    }
+};
+
+FrozenState.prototype.reduceTimes = function() {
+    this.frozenStatus.pop();
+};
+
+module.exports = FrozenState;
